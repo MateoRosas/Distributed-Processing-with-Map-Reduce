06c89ca20f0014547ee71ff18f6ed18e0b421ad8
root <mateosrosasm@outlook.es>
2022-03-06 04:40:45 +0000

    Punto 1 completado y codigo actualizado

--- bigdata/corte1/code/mapper_p1.py
+++ bigdata/corte1/code/mapper_p1.py
@@ -2,14 +2,27 @@
 import sys
 
 cabecera = sys.stdin.readline()
-print(cabecera.strip().split(","))
+#print(cabecera.strip().split(","))
 countYear = []
 dateTransfer =[]
+years_s = [str(i) for i in range(1994, 2018, 1)]
+dict1 = {}
+def manageYear(date):
+  date_s, hour = date.strip().split(" ")
+  year, month, day = date_s.strip().split("-")
+  return year
+
+
 for line in sys.stdin:
   arreglo = line.strip().split(",")
-  dateTransfer.append(arreglo[2])
+  countYear.append(manageYear(arreglo[2]))
+  counter = len(countYear) - 1
+  if countYear[counter] in dict1: 
+    dict1[countYear[counter]] += 1
+  else:
+    dict1[countYear[counter]] = 1
+  
+for key, values in dict1.iteritems():
+  print(key, values)
 
-for i in dateTransfer:
-  date, hour = i.strip().split(" ")
-  year, month, day = date.strip().split("-")
-  countYear.append(year)
+
