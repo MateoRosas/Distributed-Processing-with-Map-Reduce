d11f3bbbd43a10e96bbb223a71dbd616e3896dfa
root <mateosrosasm@outlook.es>
2022-03-06 19:57:25 +0000

    Version mejorada del script 6 y resultados de ejecucion p6

--- bigdata/corte1/code/mapper_p6.py
+++ bigdata/corte1/code/mapper_p6.py
@@ -7,18 +7,20 @@ dic_county = {}
 dic_city = {}
 dic_prueba = {}
 
-print(cabecera.strip().split(","))
+#print(cabecera.strip().split(","))
 for line in sys.stdin:
   arreglo = line.strip().split(",")
   #print(arreglo[6], arreglo[8])
   if arreglo[6] in dic_county:
     dic_county[arreglo[6]] += 1
-    dic_prueba[arreglo[6]] += " , "+arreglo[8]
+    if arreglo[8] in dic_city:
+      dic_city[arreglo[8]] += 1
+    else:
+      dic_city[arreglo[8]] = 1
+    dic_prueba[arreglo[6]] = dic_city.values()
   else:
     dic_county[arreglo[6]] = 1
-    dic_prueba[arreglo[6]] = arreglo[8]
+    #dic_prueba[arreglo[6]] = dic_city.values(), dic_city.keys()
   
-  if arreglo[8] in dic_city:
-    dic_city[arreglo[8]] += 1
-  else:
-    dic_city[arreglo[8]] = 1
+for key, values in dic_prueba.iteritems():
+  print(key, values)
