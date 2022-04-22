cf12a36466eb5d079bb8da1bb37ca4d4074f8923
root <mateosrosasm@outlook.es>
2022-04-22 05:14:30 +0000

    mejora punto 5

--- bigdata/corte1/code/solucion_p5_p6/reducerP5.py
+++ bigdata/corte1/code/solucion_p5_p6/reducerP5.py
@@ -1,6 +1,6 @@
 import sys
 
-current_year = None
+current_word = None
 current_count = 0
 word = None
 count = 0
@@ -25,12 +25,31 @@ for line in sys.stdin:
    count = int(count[1])
    sum = int(sum)
    #print(year, month, sum, count)
-   dict1[word] = maxN(max, sum)
+   dict1[word] = sum
    if word in dict1:
-     dict1[word] = maxN(max, sum)
-     #dict1[word] = sum
-     #print(dict1[word], sum)
+     sum_1 += sum
+     dict1[word] = sum_1
 
-for key, values in dict1.items():
-  print(key,values,count)
+dict2 = {}
+mayor = 0
+name = None
+for key, value in dict1.items():
+   count += 1
+   if current_word == key:
+     current_count += count
+   else:
+     if current_word:
+       if max < current_count:
+         name = current_word
+         max = current_count
+         mayor = value
+       if(current_word[5:-1] == "12" or current_word == "2017-05 "):
+         print(name, mayor, "1")         
+     current_word = key
+     current_count = count
 
+#for key, value in dict2.items():
+#   print(key, value, count)
+
+if current_word == word:
+  print(current_word, mayor, "1")
