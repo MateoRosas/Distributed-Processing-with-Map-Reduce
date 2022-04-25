bebb727d66c2f3933f4aa80e942a692231249e39
root <mateo.rosas01@outlook.es>
2022-04-25 05:30:09 +0000

    P5 succes

--- bigdata/corte1/code/solucion_p5_p6/reducer_punto5.py
+++ bigdata/corte1/code/solucion_p5_p6/reducer_punto5.py
@@ -7,19 +7,22 @@ count_ = 0
 max = 0
 data = {}
 mayor = 0
+current_word = None
+current_value = 0
+
 for line in sys.stdin:
    word, count, sum = line.split("\t")
    year, month = word.split("-")
    current_date = word
-   current_value = int(sum)
-   data[current_date] = 0
-   if current_date in data:
-     mayor += current_value
-     data[current_date] = mayor
+   #current_value = int(sum)
+   if current_word == word:
+      mayor += int(sum)
+      data[current_word] = mayor
    else:
      mayor = 0
-   
+     current_word = word
+     #current_value = int(sum)
 
-#dict(filter(lambda x: x[1] > 1, data.items()))
-largest_num = reduce(lambda x, y: x if x>y else y, data.values())
-print(largest_num)
+
+for key, values in data.items():
+    print(key, values)
